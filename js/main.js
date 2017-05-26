09197a3df554389b18d77c91b29afed85d28557b
Vadim Korabelnik <vad@oblako9.ru>
2017-05-27 02:16:42 +0300

    Changed forEach function to map

--- js/main.js
+++ js/main.js
@@ -5,11 +5,10 @@
   let footer = document.querySelector(`.central .footer`);
   let greeting = document.querySelector(`#greeting`);
   let temps = document.querySelectorAll(`template`);
-  let tempsArr = [];
-  let linksArr = [];
+  const tempsArr = [];
   Array.prototype.push.apply(tempsArr, temps);
-  tempsArr.forEach(function (temp) {
-    linksArr.push(temp.content.querySelectorAll(`img[alt='Next'], img[alt='Back']`)); // Pushing all links Next and Back to temps array
+  const linksArr = tempsArr.map(function (temp) {
+    return temp.content.querySelectorAll(`img[alt='Next'], img[alt='Back']`); // Pushing all links Next and Back to temps array
   });
   function show(slide) {
     let mainArr = [main, footer];
