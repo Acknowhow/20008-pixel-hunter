3294325a2f8253c56a5bbe53bca8195d6b5f3970
Vadim Korabelnik <vad@oblako9.ru>
2017-05-26 04:33:42 +0300

    Pushed all links to array and displayed greeting

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 
 (function () {
-  let main = document.querySelector(`main`);
-  let els = document.getElementsByTagName(`template`);
-  let arr = [];
-  main.style.display = `none`;
-  Array.prototype.push.apply(arr, els);
-  arr[0].classList.add(`greeting`);
-  arr[0].style.display = `block`;
+  let central = document.querySelector(`.central`);
+  let main = document.querySelector(`#main`);
+  let footer = document.querySelector(`.central .footer`);
+  let greeting = document.querySelector(`#greeting`);
+  let temps = document.querySelectorAll(`template`);
+  let tempsArr = [];
+  let linksArr = [];
+  Array.prototype.push.apply(tempsArr, temps);
+  tempsArr.forEach(function (temp) {
+    linksArr.push(temp.content.querySelectorAll(`img[alt='Next'], img[alt='Back']`)); // Pushing all links Next and Back to temps array
+  });
+  function show(slide) {
+    let mainArr = [main, footer];
+    let content = slide.content;
+    let clone = document.importNode(content, true); // Clone template content
+    mainArr.forEach(function (selector) {
+      selector.style.display = `none`;
+    });
+    central.appendChild(clone);
+  }
+  show(greeting); // Greeting
 })();
 
