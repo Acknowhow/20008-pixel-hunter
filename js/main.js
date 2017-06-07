6d2831612db6ba34c13f4794c5d587b42e680c10
Vadim Korabelnik <vad@oblako9.ru>
2017-06-08 02:12:15 +0300

    Added function for switching between templates

--- js/main.js
+++ js/main.js
@@ -1,38 +1,41 @@
 import constructor from './modules/module_constructor.js';
 
 (function () {
-
-  // let central = document.querySelector(`.central`);
-  // let main = document.querySelector(`#main`);
-  // // let footer = document.querySelector(`.central .footer`);
-  // let greeting = document.querySelector(`#greeting`);
-  // let temps = document.querySelectorAll(`template`);
-  // let tempsArr = [];
+  let main = document.querySelector(`#main`);
+  let central = document.querySelector(`.central`);
+  let temps = document.querySelectorAll(`template`);
+  let tempsArr = [];
+  let linksArr = [];
+  // Function for making modules
   constructor();
-
-  // document.querySelector(`#main`).innerHTML = moduleIntro;
-  // document.querySelector(`#greeting`).innerHTML = moduleGreeting;
-  // document.querySelector(`#rules`).innerHTML = moduleRules;
-  // document.querySelector(`#game-1`).innerHTML = moduleGame1;
-  // document.querySelector(`#game-2`).innerHTML = moduleGame2;
-  // document.querySelector(`#game-3`).innerHTML = moduleGame3;
-  // document.querySelector(`#stats`).innerHTML = moduleStats;
-
-
-  // Array.prototype.push.apply(tempsArr, temps);
-  // tempsArr.map(function (temp) {
-  //   // Pushing all links Next and Back to temps array for future use
-  //   return temp.content.querySelectorAll(`img[alt='Next'], img[alt='Back']`);
-  // });
-  // function show(slide) {
-  //   // let mainArr = [main, footer];
-  //   let content = slide.content;
-  //   // Clone template content
-  //   let clone = document.importNode(content, true);
-  //   main.style.display = `none`;
-  //   central.insertBefore(clone, central.firstChild);
+  Array.prototype.push.apply(tempsArr, temps);
+  linksArr = tempsArr.map(function (temp) {
+    // Pushing all links Next and Back to temps array for future use
+    return temp.content.querySelectorAll(`img[alt='Next'], img[alt='Back']`);
+  });
+  const show = (slide) => {
+    // let mainArr = [main, footer];
+    let content = slide.content;
+    // Clone template content
+    let clone = document.importNode(content, true);
+    central.children[0].style.display = `none`;
+    central.insertBefore(clone, central.children[0]);
+  }
+  // Greeting
+  central.addEventListener(`click`, function (e) {
+    if (e.target === document.querySelector(`.intro__asterisk`)) {
+      show(document.querySelector(`#greeting`));
+    }
+    if (e.target === document.querySelector(`img[alt='Next']`)) {
+      show(document.querySelector(`#rules`));
+    }
+    return false;
+  });
+  // for (let i = 0; i++; i < tempsArr.length) {
+  //   if (tempsArr[i][0].alt === `Next`) {
+  //     document.querySelector(`img`).addEventListener(`click`, function () {
+  //       show(document.)
+  //     })
+  //   }
   // }
-  // // Greeting
-  // show(greeting);
 })();
-
