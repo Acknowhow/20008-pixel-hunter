f27b092c7a4afa1707b7c16561fe2514d502a062
Vadim Korabelnik <vad@oblako9.ru>
2017-06-07 21:07:36 +0300

    Created a separate module for displaying modules

--- js/main.js
+++ js/main.js
@@ -1,43 +1,38 @@
-import moduleIntro from './modules/templates/intro.js';
-import moduleGreeting from './modules/templates/greeting.js';
-import moduleRules from './modules/templates/rules.js';
-import moduleGame1 from './modules/templates/game-1.js';
-import moduleGame2 from './modules/templates/game-2.js';
-import moduleGame3 from './modules/templates/game-3.js';
-import moduleStats from './modules/templates/stats.js';
+import constructor from './modules/module_constructor.js';
 
 (function () {
 
-  let central = document.querySelector(`.central`);
-  let main = document.querySelector(`#main`);
-  // let footer = document.querySelector(`.central .footer`);
-  let greeting = document.querySelector(`#greeting`);
-  let temps = document.querySelectorAll(`template`);
-  let tempsArr = [];
+  // let central = document.querySelector(`.central`);
+  // let main = document.querySelector(`#main`);
+  // // let footer = document.querySelector(`.central .footer`);
+  // let greeting = document.querySelector(`#greeting`);
+  // let temps = document.querySelectorAll(`template`);
+  // let tempsArr = [];
+  constructor();
 
-  document.querySelector(`#main`).innerHTML = moduleIntro;
-  document.querySelector(`#greeting`).innerHTML = moduleGreeting;
-  document.querySelector(`#rules`).innerHTML = moduleRules;
-  document.querySelector(`#game-1`).innerHTML = moduleGame1;
-  document.querySelector(`#game-2`).innerHTML = moduleGame2;
-  document.querySelector(`#game-3`).innerHTML = moduleGame3;
-  document.querySelector(`#stats`).innerHTML = moduleStats;
+  // document.querySelector(`#main`).innerHTML = moduleIntro;
+  // document.querySelector(`#greeting`).innerHTML = moduleGreeting;
+  // document.querySelector(`#rules`).innerHTML = moduleRules;
+  // document.querySelector(`#game-1`).innerHTML = moduleGame1;
+  // document.querySelector(`#game-2`).innerHTML = moduleGame2;
+  // document.querySelector(`#game-3`).innerHTML = moduleGame3;
+  // document.querySelector(`#stats`).innerHTML = moduleStats;
 
 
-  Array.prototype.push.apply(tempsArr, temps);
-  tempsArr.map(function (temp) {
-    // Pushing all links Next and Back to temps array for future use
-    return temp.content.querySelectorAll(`img[alt='Next'], img[alt='Back']`);
-  });
-  function show(slide) {
-    // let mainArr = [main, footer];
-    let content = slide.content;
-    // Clone template content
-    let clone = document.importNode(content, true);
-    main.style.display = `none`;
-    central.insertBefore(clone, central.firstChild);
-  }
-  // Greeting
-  show(greeting);
+  // Array.prototype.push.apply(tempsArr, temps);
+  // tempsArr.map(function (temp) {
+  //   // Pushing all links Next and Back to temps array for future use
+  //   return temp.content.querySelectorAll(`img[alt='Next'], img[alt='Back']`);
+  // });
+  // function show(slide) {
+  //   // let mainArr = [main, footer];
+  //   let content = slide.content;
+  //   // Clone template content
+  //   let clone = document.importNode(content, true);
+  //   main.style.display = `none`;
+  //   central.insertBefore(clone, central.firstChild);
+  // }
+  // // Greeting
+  // show(greeting);
 })();
 
---
+++ js/modules/module_constructor.js
@@ -0,0 +1,21 @@
+import moduleIntro from './templates/intro.js';
+import moduleGreeting from './templates/greeting.js';
+import moduleRules from './templates/rules.js';
+import moduleGame1 from './templates/game-1.js';
+import moduleGame2 from './templates/game-2.js';
+import moduleGame3 from './templates/game-3.js';
+import moduleStats from './templates/stats.js';
+
+const constructor = () => {
+  document.querySelector(`#main`).innerHTML = moduleIntro;
+  document.querySelector(`#greeting`).innerHTML = moduleGreeting;
+  document.querySelector(`#rules`).innerHTML = moduleRules;
+  document.querySelector(`#game-1`).innerHTML = moduleGame1;
+  document.querySelector(`#game-2`).innerHTML = moduleGame2;
+  document.querySelector(`#game-3`).innerHTML = moduleGame3;
+  document.querySelector(`#stats`).innerHTML = moduleStats;
+};
+
+export default constructor;
+
+
