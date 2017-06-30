fc20bc158595d811f238c748165f4530cb956123
Vadim Korabelnik <vad@oblako9.ru>
2017-06-30 20:36:11 +0300

    Making separate footer

--- js/modules/module_constructor.js
+++ js/modules/module_constructor.js
@@ -1,7 +1,9 @@
+import {footerContent} from './../footer.js';
 export const central = document.querySelector(`.central`);
 // Function for inserting screens directly into container
 export const makeTemplate = (module) =>{
   const container = document.createElement(`template`);
+  const footer = document.createElement(`footer`);
   container.innerHTML = module;
   while (central.firstChild) {
     central.removeChild(central.firstChild);
