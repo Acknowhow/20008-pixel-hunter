f8eae0c746a746f2f982edd266e0ee809ff66ece
Vadim Korabelnik <vad@oblako9.ru>
2017-07-09 16:24:42 +0300

    Now make map structure for images and sort them randomly onload

--- js/modules/templates/data/imagesData.js
+++ js/modules/templates/data/imagesData.js
@@ -1,20 +1,46 @@
-export default {
-  paintings: [
-    // People
-    // Beardman
-    `http://i.imgur.com/5kcp9i0.jpg`,
-    // Animals
-    `https://k42.kn3.net/D2F0370D6.jpg`,
-    // Nature
-    `https://k32.kn3.net/5C7060EC5.jpg`
+export const paintings = {
+  people: [
+    [`Man bearded`, `http://i.imgur.com/5kcp9i0.jpg`],
+    [`Woman showered`, `http://i.imgur.com/l8J6vbh.jpg`],
+    [`Woman thoughtful`, `http://i.imgur.com/aA3KkAW.jpg`],
+    [`Girls sitting`, `http://i.imgur.com/27nbLSA.jpg`]
   ],
-  photos: [
-    // People
-    `http://i.imgur.com/egBvykv.jpg`,
-    // Animals
-    `https://i.imgur.com/DiHM5Zb.jpg`,
-    // Nature
-    `http://i.imgur.com/DKR1HtB.jpg`
+  animals: [
+    [`Lizard`, `http://i.imgur.com/FA4D3kO.jpg`],
+    [`Spider`, `http://i.imgur.com/kIyT2iO.jpg`]
+  ],
+  items: [
+    [`Metal spheres`, `http://i.imgur.com/Sjjnuyi.jpg`]
+  ],
+  fruits: [
+    [`Watermelon`, `http://i.imgur.com/rvDgwaA.jpg`],
+    [`Pomegranade`, `http://i.imgur.com/mgbDqkB.jpg`]
+  ],
+  dishes: [
+    [`Plates`, `http://i.imgur.com/cjS2DDf.jpg`]
   ]
 };
+export const photos = {
+  people: [
+    [`Morgan Freeman`, `http://i.imgur.com/8aNXpLk.jpg`],
+    [`Grandma Indian`, `http://i.imgur.com/PKtMBLt.jpg`],
+    [`Girl homeless`, `http://i.imgur.com/mdvy2jt.jpg`],
+    [`Ethyopian girl`, `http://i.imgur.com/jX3HIry.jpg`]
+  ],
+  animals: [
+    [`Bumble bee`, `http://i.imgur.com/OI49pzH.jpg`],
+    [`Cheetah`, `http://i.imgur.com/LWFCQSK.jpg`]
+  ],
+  items: [
+    [`Books abstract`, `http://i.imgur.com/Eb5bc8u.jpg)`]
+  ],
+  fruits: [
+    [`Figs`, `http://i.imgur.com/y9cN0UD.jpg`],
+    [`Apple tree`, `http://i.imgur.com/qDpBXvT.jpg`]
+  ],
+  dishes: [
+    [`Mug`, `http://i.imgur.com/Spk7kTG.jpg`]
+  ]
+}
+
 
